97a1137ecd259ea86c05dd4bf67c48a824f0a67b
Jarnovhouten <144031973+Jarnovhouten@users.noreply.github.com>
2025-02-17 15:26:06 +0100

    Delete helper.py

--- helper.py
+++
@@ -1,8 +0,0 @@
-import sagemaker
-
-def get_endpoint_status():
-    try:
-        response = sagemaker_client.describe_endpoint(EndpointName=endpoint_name)
-        return response['EndpointStatus']
-    except sagemaker_client.exceptions.ClientError:
-        return None
